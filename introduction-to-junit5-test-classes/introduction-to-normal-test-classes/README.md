8e3230d976b1737481af143d3c48c1d5ed64dfd4
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-02-07 19:29:11 +0200

    Add example: Introduction to "Normal" Test Classes

---
+++ introduction-to-junit5-test-classes/introduction-to-normal-test-classes/README.md
@@ -0,0 +1,17 @@
+# Introduction to "Normal" Test Classes
+
+This is an example project of a lesson that describes how you can:
+
+* Get the required dependencies with Maven.
+
+[Watch (or read) the lesson: Introduction to "Normal" Test Classes](https://www.cleantestautomation.com/lessons/introduction-to-normal-test-classes/)
+
+When you want to run the tests found from this project with Maven, you have to run the
+following command at command prompt:
+
+        mvn clean test
+
+When you want to run the tests found from this project with Gradle, you have to run the
+following command at command prompt: 
+
+        gradle clean test
---
+++ introduction-to-junit5-test-classes/introduction-to-normal-test-classes/src/test/java/com/cleantestautomation/junit5intro/JUnit5ExampleTest.java
@@ -0,0 +1,63 @@
+package com.cleantestautomation.junit5intro;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test class demonstrates the execution order of
+ * setup, teardown, and test methods.
+ */
+@DisplayName("JUnit 5 Example")
+class JUnit5ExampleTest {
+
+    /**
+     * This setup method is run once before any test
+     * method is run.
+     */
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
+    }
+
+    /**
+     * This setup method is run before a test method is run.
+     */
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+
+    /**
+     * This teardown method is run after a test method has been
+     * run.
+     */
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+
+    /**
+     * This teardown method is run once after all test methods
+     * have been run.
+     */
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }
+
+    @Test
+    @DisplayName("First test")
+    void firstTest() {
+        System.out.println("First test method");
+    }
+
+    @Test
+    @DisplayName("Second test")
+    void secondTest() {
+        System.out.println("Second test method");
+    }
+}
