f276fae6c41cb72f65c2616a7b5eed288a9e081b
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-02-11 15:45:01 +0200

    Add example: Introduction to Nested Test Classes

---
+++ introduction-to-junit5/introduction-to-junit5-test-classes/introduction-to-nested-test-classes/README.md
@@ -0,0 +1,19 @@
+# Introduction to Nested Test Classes
+
+This is an example project of a lesson that helps you to:
+ 
+* Create nested test classes with JUnit 5.
+* Add setup, teardown, and test methods to nested test classes. 
+* Understand the invocation order of setup, teardown, and test methods.
+
+[Watch (or read) the lesson: Introduction to Nested Test Classes](https://www.cleantestautomation.com/lessons/introduction-to-nested-test-classes/)
+
+When you want to run the tests found from this project with Maven, you have to run the
+following command at command prompt:
+
+        mvn clean test
+
+When you want to run the tests found from this project with Gradle, you have to run the
+following command at command prompt: 
+
+        gradle clean test
---
+++ introduction-to-junit5/introduction-to-junit5-test-classes/introduction-to-nested-test-classes/src/test/java/com/cleantestautomation/junit5intro/JUnit5NestedExampleTest.java
@@ -0,0 +1,128 @@
+package com.cleantestautomation.junit5intro;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test class demonstrates the invocation order of
+ * setup, teardown, and test methods when you are writing
+ * nested tests with JUnit 5.
+ */
+@DisplayName("JUnit 5 Example")
+class JUnit5NestedExampleTest {
+
+    /**
+     * This setup method is run once before any test
+     * method is run.
+     */
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
+    }
+
+    /**
+     * This setup method is run before a test method is run.
+     */
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+
+    /**
+     * This teardown method is run after a test method has been
+     * run.
+     */
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+
+    /**
+     * This teardown method is run once after all test methods
+     * have been run.
+     */
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }
+
+    @Test
+    @DisplayName("First test")
+    void firstTest() {
+        System.out.println("First test method");
+    }
+
+    @Test
+    @DisplayName("Second test")
+    void secondTest() {
+        System.out.println("Second test method");
+    }
+
+    @Nested
+    @DisplayName("Tests for the method A")
+    class A {
+
+        @BeforeEach
+        void beforeEach() {
+            System.out.println("Before each test method of the A class");
+        }
+
+        @AfterEach
+        void afterEach() {
+            System.out.println("After each test method of the A class");
+        }
+
+        @Test
+        @DisplayName("Example test for method A")
+        void sampleTestForMethodA() {
+            System.out.println("Example test for method A");
+        }
+
+        @Nested
+        @DisplayName("When X is true")
+        class WhenX {
+
+            @BeforeEach
+            void beforeEach() {
+                System.out.println("Before each test method of the WhenX class");
+            }
+
+            @AfterEach
+            void afterEach() {
+                System.out.println("After each test method of the WhenX class");
+            }
+
+            @Test
+            @DisplayName("Example test for method A when X is true")
+            void sampleTestForMethodAWhenX() {
+                System.out.println("Example test for method A when X is true");
+            }
+        }
+
+        @Nested
+        @DisplayName("When Y is true")
+        class WhenY {
+
+            @BeforeEach
+            void beforeEach() {
+                System.out.println("Before each test method of the WhenY class");
+            }
+
+            @AfterEach
+            void afterEach() {
+                System.out.println("After each test method of the WhenY class");
+            }
+
+            @Test
+            @DisplayName("Example test for method A when Y is true")
+            void sampleTestForMethodAWhenY() {
+                System.out.println("Example test for method A when Y is true");
+            }
+        }
+    }
+}
