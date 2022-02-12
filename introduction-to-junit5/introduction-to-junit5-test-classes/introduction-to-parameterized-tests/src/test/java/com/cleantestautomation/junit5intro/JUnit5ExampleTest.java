a40cf77509882e571c3e6a22912a4511b4d8f12d
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-02-12 13:21:07 +0200

    Add example: Introduction to Parameterized Tests

---
+++ introduction-to-junit5/introduction-to-junit5-test-classes/introduction-to-parameterized-tests/src/test/java/com/cleantestautomation/junit5intro/JUnit5ExampleTest.java
@@ -0,0 +1,30 @@
+package com.cleantestautomation.junit5intro;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * This test class demonstrates how you can write a
+ * simple parameterized test with JUnit 5 and customize
+ * the display name of each method invocation.
+ */
+@DisplayName("JUnit 5 Parameterized Test Example")
+class JUnit5ExampleTest {
+
+    @DisplayName("Should pass a non-null message to our test method")
+    @ParameterizedTest
+    @ValueSource(strings = {"Hello", "World"})
+    void shouldPassNonNullMessageAsMethodParameter(String message) {
+        assertNotNull(message);
+    }
+
+    @DisplayName("Should pass a non-null message to our test method")
+    @ParameterizedTest(name = "{index} => message=''{0}''")
+    @ValueSource(strings = {"Hello", "World"})
+    void shouldPassNonNullMessageAsMethodParameterWithCustomInvocationDescription(String message) {
+        assertNotNull(message);
+    }
+}
