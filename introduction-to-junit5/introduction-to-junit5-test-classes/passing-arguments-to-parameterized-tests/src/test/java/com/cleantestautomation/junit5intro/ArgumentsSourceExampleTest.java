fead22181dd8cc20992a0e4066ceb40c4ea5e520
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-02-15 18:36:12 +0200

    Remove unused imports

--- introduction-to-junit5/introduction-to-junit5-test-classes/passing-arguments-to-parameterized-tests/src/test/java/com/cleantestautomation/junit5intro/ArgumentsSourceExampleTest.java
+++ introduction-to-junit5/introduction-to-junit5-test-classes/passing-arguments-to-parameterized-tests/src/test/java/com/cleantestautomation/junit5intro/ArgumentsSourceExampleTest.java
@@ -3,8 +3,6 @@ package com.cleantestautomation.junit5intro;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.util.stream.Stream;
