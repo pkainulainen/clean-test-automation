e4f31ce120faca716ac7117889a83bae6f3ca2b2
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-03-24 19:09:36 +0200

    Fix custom error messages

--- introduction-to-junit5/writing-assertions-with-junit5/writing-custom-assertions-with-assertj/src/test/java/com/cleantestautomation/junit5intro/TodoItemAssertion.java
+++ introduction-to-junit5/writing-assertions-with-junit5/writing-custom-assertions-with-assertj/src/test/java/com/cleantestautomation/junit5intro/TodoItemAssertion.java
@@ -48,21 +48,22 @@ public class TodoItemAssertion {
 
         softAssertions.assertThat(actual.getStatus())
                 .overridingErrorMessage(
-                        "The status of an open task must be: %s",
-                        TodoItemStatus.OPEN
+                        "Expected that the status is: %s but was: %s",
+                        TodoItemStatus.OPEN,
+                        actual.getStatus()
                 )
                 .isEqualTo(TodoItemStatus.OPEN);
 
         softAssertions.assertThat(actual.getResolution())
                 .overridingErrorMessage(
-                        "Expected that the resolution of an open task is null but was: %s",
+                        "Expected that the resolution is null but was: %s",
                         actual.getStatus()
                 )
                 .isNull();
 
         softAssertions.assertThat(actual.getCloserId())
                 .overridingErrorMessage(
-                        "Expected that the closerId of an open task is null but was: %d",
+                        "Expected that the closerId is null but was: %d",
                         actual.getCloserId()
                 )
                 .isNull();
@@ -84,7 +85,7 @@ public class TodoItemAssertion {
 
         softAssertions.assertThat(actual.getStatus())
                 .overridingErrorMessage(
-                        "Expected thatthe status to be: %s but was: %s",
+                        "Expected that the status is: %s but was: %s",
                         TodoItemStatus.CLOSED,
                         actual.getStatus()
                 )
