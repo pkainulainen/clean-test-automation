5e9e767a93d530fcc4644fce3bca6aee2f8f6919
Petri Kainulainen <petri.kainulainen@gmail.com>
2022-10-22 10:15:59 +0300

    Rewrite tests which ensure that Optional contains a value

--- introduction-to-junit5/writing-assertions-with-junit5/writing-basic-assertions-with-junit5/src/test/java/com/cleantestautomation/junit5intro/OptionalAssertionTest.java
+++ introduction-to-junit5/writing-assertions-with-junit5/writing-basic-assertions-with-junit5/src/test/java/com/cleantestautomation/junit5intro/OptionalAssertionTest.java
@@ -39,23 +39,23 @@ class OptionalAssertionTest {
     }
 
     @Nested
-    @DisplayName("When the optional is not empty")
-    class WhenOptionalIsNotEmpty {
+    @DisplayName("When the optional contains a value")
+    class WhenOptionalContainsValue {
 
         private final String STRING = "object";
 
         @Test
-        @DisplayName("Should not be empty")
-        void shouldNotBeEmpty() {
+        @DisplayName("Should contain a value")
+        void shouldContainValue() {
             final Optional<Object> actual = Optional.of(STRING);
-            assertFalse(actual.isEmpty());
+            assertTrue(actual.isPresent());
         }
 
         @Test
-        @DisplayName("Should not be empty (with custom error message)")
-        void shouldNotBeEmptyWithCustomErrorMessage() {
+        @DisplayName("Should contain a value (with custom error message)")
+        void shouldContainValueWithCustomErrorMessage() {
             final Optional<Object> actual = Optional.of(STRING);
-            assertFalse(actual.isEmpty(),
+            assertTrue(actual.isPresent(),
                     "Expected optional to not be empty but it was empty"
             );
         }
