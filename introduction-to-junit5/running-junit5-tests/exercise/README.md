d9b1b3129fb8e24de1f512dd64d62eb7b5c3ac5c
Petri Kainulainen <petri.kainulainen@gmail.com>
2023-06-06 19:26:16 +0300

    Add exercise: Running Tests With Maven/Gradle

---
+++ introduction-to-junit5/running-junit5-tests/exercise/README.md
@@ -0,0 +1,3 @@
+# Exercise: Running Tests With Maven/Gradle
+
+Check the instructions from the lesson.
---
+++ introduction-to-junit5/running-junit5-tests/exercise/src/test/java/com/cleantestautomation/junit5intro/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.cleantestautomation.junit5intro;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    void helloWorld() {
+        System.out.println("Hello World!");
+    }
+}
