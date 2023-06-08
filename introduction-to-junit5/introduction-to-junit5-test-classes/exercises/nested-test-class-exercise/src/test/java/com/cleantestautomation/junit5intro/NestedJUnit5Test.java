243197466576fa0ec551d019c038cad3dd8fe910
Petri Kainulainen <petri.kainulainen@gmail.com>
2023-06-08 19:22:29 +0300

    Add exercise: Create a Nested Test Class

---
+++ introduction-to-junit5/introduction-to-junit5-test-classes/exercises/nested-test-class-exercise/src/test/java/com/cleantestautomation/junit5intro/NestedJUnit5Test.java
@@ -0,0 +1,27 @@
+package com.cleantestautomation.junit5intro;
+
+import org.junit.jupiter.api.*;
+
+@DisplayName("Nested test class exercise")
+class NestedJUnit5Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }
+}
