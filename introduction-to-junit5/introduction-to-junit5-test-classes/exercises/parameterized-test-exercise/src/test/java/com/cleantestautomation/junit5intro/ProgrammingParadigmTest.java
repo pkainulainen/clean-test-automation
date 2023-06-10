59812af99d69ecd2d97fbd53d7ad6caa6c647433
Petri Kainulainen <petri.kainulainen@gmail.com>
2023-06-10 15:27:43 +0300

    Remove unused import

--- introduction-to-junit5/introduction-to-junit5-test-classes/exercises/parameterized-test-exercise/src/test/java/com/cleantestautomation/junit5intro/ProgrammingParadigmTest.java
+++ introduction-to-junit5/introduction-to-junit5-test-classes/exercises/parameterized-test-exercise/src/test/java/com/cleantestautomation/junit5intro/ProgrammingParadigmTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
