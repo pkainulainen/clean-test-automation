24ecefe51ff8ab6183753bde627fe88d8cba1b29
Petri Kainulainen <petri.kainulainen@gmail.com>
2023-06-10 15:27:24 +0300

    Add calculator example

---
+++ introduction-to-junit5/introduction-to-junit5-test-classes/exercises/parameterized-test-exercise/src/main/java/com/cleantestautomation/junit5intro/Calculator.java
@@ -0,0 +1,16 @@
+package com.cleantestautomation.junit5intro;
+
+/**
+ * A simple calculator
+ */
+public class Calculator {
+
+    /**
+     * Returns the sum of two numbers.
+     * @param a The first number
+     * @param b The second number.
+     */
+    public static long sum(long a, long b) {
+        return a + b;
+    }
+}
---
+++ introduction-to-junit5/introduction-to-junit5-test-classes/exercises/parameterized-test-exercise/src/test/java/com/cleantestautomation/junit5intro/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.cleantestautomation.junit5intro;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Tests for the Calculator class")
+class CalculatorTest {
+
+    @Nested
+    @DisplayName("Returns the sum of two numbers")
+    class Sum {
+
+        @DisplayName("Should return the correct sum")
+        @ParameterizedTest(name = "{0} + {1} must be: {2}")
+        void shouldReturnCorrectSum(long a, long b, long expectedSum) {
+            long actualSum = Calculator.sum(a, b);
+            assertThat(actualSum).isEqualByComparingTo(expectedSum);
+        }
+    }
+}
