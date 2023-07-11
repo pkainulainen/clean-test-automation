426bf3e4b87a2144698ff8c475fa30dc0ffdd612
Petri Kainulainen <petri.kainulainen@gmail.com>
2023-07-11 10:47:09 +0300

    Remove unnecessary tests and clean up code

--- introduction-to-assertj-db/writing-assertions-with-assertj-db/writing-soft-assertions/src/test/java/com/cleantestautomation/assertjdb/todoitem/CreateTodoItemTest.java
+++ introduction-to-assertj-db/writing-assertions-with-assertj-db/writing-soft-assertions/src/test/java/com/cleantestautomation/assertjdb/todoitem/CreateTodoItemTest.java
@@ -13,11 +13,9 @@ import org.springframework.test.context.jdbc.Sql;
 
 import javax.sql.DataSource;
 
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-
 /**
- * This exercise helps you understand how you can write assertions which ensure that
- * the correct information is inserted into a relational database.
+ * This exercise helps you understand how you can write soft assertions for the data
+ * that's found from a relational database.
  */
 @SpringBootTest
 @ActiveProfiles("integrationTest")
@@ -55,96 +53,10 @@ class CreateTodoItemTest {
     }
 
     @Test
-    @DisplayName("Should insert a new todo item into the database")
-    void shouldInsertNewTodoItemIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the todo_item table has one row.
-    }
-
-    @Test
-    @DisplayName("Should use the next free id when a new todo is inserted into the database")
-    void shouldUseNextFreeIdWhenNewTodoItemIsInsertedIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the next free id is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct creator ID into the database")
-    void shouldInsertCorrectCreatorIdIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the correct creator id is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct creation time into the database")
-    void shouldInsertCorrectCreationTimeIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the correct creation time is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct description into the database")
-    void shouldInsertCorrectDescriptionIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the correct description is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct modification time into the database")
-    void shouldInsertCorrectModificationTimeIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the correct modification time is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct modifier ID into the database")
-    void shouldInsertCorrectModifierIdIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the correct modifier id is inserted into the database.
-    }
-
-    @Test
     @DisplayName("Should insert an open todo item into the database")
     void shouldInsertOpenTodoItemIntoDatabase() {
         repository.create(INPUT);
         //TODO: Write the soft assertions which ensure that an open todo item is inserted into the database.
         //HINT: A todo is open when its status is: TodoItemStatus.OPEN and resolution is null.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct title into the database")
-    void shouldInsertCorrectTitleIntoDatabase() {
-        repository.create(INPUT);
-        //TDDO: Write the assertions which ensure that the correct title is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should insert the correct version into the database")
-    void shouldInsertCorrectVersionIntoDatabase() {
-        repository.create(INPUT);
-        //TODO: Write the assertion which ensures that the correct version is inserted into the database.
-    }
-
-    @Test
-    @DisplayName("Should return the information of the created todo item")
-    void shouldReturnInformationOfCreatedTodoItem() {
-        var created = repository.create(INPUT);
-        assertSoftly(softAssertions -> {
-            softAssertions.assertThat(created.getId())
-                    .as("id")
-                    .isEqualByComparingTo(IdColumnReset.NEXT_ID);
-            softAssertions.assertThat(created.getDescription())
-                    .as("description")
-                    .isEqualTo(TodoItems.ReadAllLessons.DESCRIPTION);
-            softAssertions.assertThat(created.getResolution())
-                    .as("resolution")
-                    .isNull();
-            softAssertions.assertThat(created.getStatus())
-                    .as("status")
-                    .isEqualTo(TodoItems.ReadAllLessons.STATUS_OPEN);
-            softAssertions.assertThat(created.getTitle())
-                    .as("title")
-                    .isEqualTo(TodoItems.ReadAllLessons.TITLE);
-        });
     }
 }
--- introduction-to-assertj-db/writing-assertions-with-assertj-db/writing-soft-assertions/src/test/java/com/cleantestautomation/assertjdb/todoitem/UpdateTodoItemTest.java
+++ introduction-to-assertj-db/writing-assertions-with-assertj-db/writing-soft-assertions/src/test/java/com/cleantestautomation/assertjdb/todoitem/UpdateTodoItemTest.java
@@ -1,6 +1,5 @@
 package com.cleantestautomation.assertjdb.todoitem;
 
-import com.cleantestautomation.assertjdb.IdColumnReset;
 import org.assertj.db.type.Table;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,11 +10,9 @@ import org.springframework.test.context.jdbc.Sql;
 
 import javax.sql.DataSource;
 
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-
 /**
- * This exercise helps you to understand how you can write the assertions which ensure
- * that 
+ * This exercise helps you to understand how you can write soft assertions for the
+ * data that's found from a relational database.
  */
 @SpringBootTest
 @ActiveProfiles("integrationTest")
@@ -47,118 +44,10 @@ class UpdateTodoItemTest {
     }
 
     @Test
-    @DisplayName("Shouldn't insert new todo items into the database")
-    void shouldNotInsertNewTodoItemsIntoDatabase() {
-        repository.update(INPUT);
-       //TODO: Write the assertion which ensures that the repository doesn't insert new todo items into the database.
-    }
-
-    @Test
-    @DisplayName("Shouldn't change the id of the updated todo item")
-    void shouldNotChangeIdOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository doesn't change the id of the updated todo item.
-    }
-
-    @Test
-    @DisplayName("Shouldn't update the creator ID of the updated todo item")
-    void shouldNotUpdateCreatorIdOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository doesn't update the creator ID of the updated
-        //todo item.
-    }
-
-    @Test
-    @DisplayName("Shouldn't update the creation time of the updated todo item")
-    void shouldNotUpdateCreationTimeOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository doesn't update the creation time of the updated
-        //todo item.
-    }
-
-    @Test
-    @DisplayName("Should update the description of the updated todo item")
-    void shouldUpdateDescriptionOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository updates the description of the updated
-        //todo item.
-    }
-
-    @Test
-    @DisplayName("Should update the modification time of the updated todo item")
-    void shouldUpdateModificationTimeOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository updates the modification time of the updated
-        //todo item.
-    }
-
-    @Test
-    @DisplayName("Should update the modifier id of the updated todo item")
-    void shouldUpdateModifierIdOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository updates the modifier id of the updated todo item.
-    }
-
-    @Test
-    @DisplayName("Shouldn't update the resolution of the updated todo item")
-    void shouldNotUpdateResolutionOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository doesn't update the resolution of the updated
-        //todo item.
-    }
-
-    @Test
-    @DisplayName("Shouldn't update the status of the updated todo item")
-    void shouldNotUpdateStatusOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository doesn't update the status of the updated
-        //todo item.
-    }
-
-    @Test
-    @DisplayName("Should update the title of the updated todo item")
-    void shouldUpdateTitleOfUpdatedTodoItem() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository updates the title of the updated todo item.
-    }
-
-    @Test
-    @DisplayName("Should increase the version of the updated todo item by one")
-    void shouldIncreaseVersionOfUpdatedTodoItemByOne() {
-        repository.update(INPUT);
-        //TODO: Write the assertion which ensures that the repository increases the version of the updated
-        //todo item by one.
-    }
-
-    @Test
-    @DisplayName("Should return the information of the updated todo item")
-    void shouldReturnInformationOfUpdatedTodoItem() {
-        var updated = repository.update(INPUT);
-        assertSoftly(softAssertions -> {
-            softAssertions.assertThat(updated.getId())
-                    .as("id")
-                    .isEqualByComparingTo(IdColumnReset.NEXT_ID);
-            softAssertions.assertThat(updated.getDescription())
-                    .as("description")
-                    .isEqualTo(TodoItems.ReadAllLessons.UPDATED_DESCRIPTION);
-            softAssertions.assertThat(updated.getResolution())
-                    .as("resolution")
-                    .isEqualTo(TodoItems.ReadAllLessons.NO_RESOLUTION);
-            softAssertions.assertThat(updated.getStatus())
-                    .as("status")
-                    .isEqualTo(TodoItems.ReadAllLessons.STATUS_OPEN);
-            softAssertions.assertThat(updated.getTitle())
-                    .as("title")
-                    .isEqualTo(TodoItems.ReadAllLessons.UPDATED_TITLE);
-        });
-    }
-
-    @Test
     @DisplayName("Shouldn't make any changes to the information of the Finish all exercises todo item")
     void shouldNotMakeAnyChangesToInformationOfFinishAllExercisesTodoItem() {
         repository.update(INPUT);
         //TODO: Write the soft assertions which ensure that the repository doesn't make any changes to the information
         //of the second todo item that's found from the database.
-
     }
 }
